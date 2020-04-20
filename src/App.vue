1ccd106b7242f769decc961f519338eb46b54444
SharyZ <onshariiit@gmail.com>
2020-04-20 20:45:40 +0500

    feat: add navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <div id="app">
+    <navbar />
     <router-view />
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+import Navbar from '@/components/Navbar.vue';
+
+@Component({
+  components: {
+    navbar: Navbar,
+  },
+})
+export default class App extends Vue {}
+</script>
 
 <style lang="scss"></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <vs-navbar>
+    <div slot="title">
+      <vs-navbar-title>
+        Music
+      </vs-navbar-title>
+    </div>
+    <vs-navbar-item index="0">
+      <vs-button to="/" color="primary" type="flat">Home</vs-button>
+    </vs-navbar-item>
+    <vs-navbar-item index="1">
+      <vs-button to="/login" color="primary" type="flat">Log in</vs-button>
+    </vs-navbar-item>
+    <vs-navbar-item index="2">
+      <vs-button to="/register" color="danger" type="flat">Sign up</vs-button>
+    </vs-navbar-item>
+    <vs-input icon="search" placeholder="Search" />
+  </vs-navbar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style scoped lang="scss">
+.vs-navbar {
+  padding: 1rem;
+}
+</style>
